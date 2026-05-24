84918f8d37ca9272305d2b8932291d25a5c3d90b
Keshara Ranasinghe <86567225+KSRanasinghe@users.noreply.github.com>
2026-05-24 08:17:11 +0530

    Initial commit

---
+++ src/test/java/com/ksr/krc_pos_backend/KrcPosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksr.krc_pos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrcPosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
